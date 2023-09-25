80cede3ecab5ee51f8cd0e5a732e461d15837ef0
dannw <danielvarelaxwx@gmail.com>
2023-09-24 18:54:53 -0600

    Actividad 7

---
+++ _p40_Numeros100a1.java
@@ -0,0 +1,13 @@
+// Imprimir los numeros del 100 al uno usando while
+
+public class _p40_Numeros100a1 {
+    public static void main(String[] args) {
+        int c;
+        c = 100;
+        while (c >= 1) {
+            System.out.print(c + " ");
+            c--;
+        }
+        System.out.println("\nProceso terminado ...");
+    }
+}
