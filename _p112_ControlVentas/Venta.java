ce2c6c80221d081aebb659ea301f826080a2a5d7
dannw <danielvarelaxwx@gmail.com>
2023-10-21 23:29:41 -0600

    Practica 19

--- _p112_ControlVentas/Venta.java
+++ _p112_ControlVentas/Venta.java
@@ -1,15 +1,40 @@
 package _p112_ControlVentas;
 
 public abstract class Venta {
-    private int Articulo;
-    private double Cantidad;
-    private double Total;
-    public Venta(int articulo, double cantidad, double total) {
+    private String Articulo;
+    private int Cantidad;
+    private double Precio;
+
+    public Venta(String articulo, int cantidad, double precio) {
         Articulo = articulo;
         Cantidad = cantidad;
-        Total = total;
+        Precio = precio;
     }
 
-    
-    
+    public String getArticulo() {
+        return Articulo;
+    }
+    public void setArticulo(String articulo) {
+        Articulo = articulo;
+    }
+    public int getCantidad() {
+        return Cantidad;
+    }
+    public void setCantidad(int cantidad) {
+        Cantidad = cantidad;
+    }
+    public double getPrecio() {
+        return Precio;
+    }
+    public void setPrecio(double precio) {
+        Precio = precio;
+    }
+    public abstract double getTotalVenta();
+
+    @Override
+    public String toString() {
+        return "Venta [Articulo=" + Articulo + ", Cantidad=" + Cantidad + ", Precio=" + Precio + ", Total=" + "]";
+    }
+
+
 }
